3d33e36f14455d5ce0199bbf910c63a9e2c182c3
Jakub Konior <karfaj45@gmail.com>
2023-03-07 23:23:37 +0100

    Add files via upload

---
+++ build/precache-manifest.8a725df033d31bbaf84b510026aebfcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3eacc636d6efbcbcd6c8",
+    "url": "./static/css/main.76d12e9d.chunk.css"
+  },
+  {
+    "revision": "3eacc636d6efbcbcd6c8",
+    "url": "./static/js/main.3eacc636.chunk.js"
+  },
+  {
+    "revision": "ed197c0f677114f68a6f",
+    "url": "./static/js/1.ed197c0f.chunk.js"
+  },
+  {
+    "revision": "ef37b8b3ea9def634c86",
+    "url": "./static/css/2.ed771ca4.chunk.css"
+  },
+  {
+    "revision": "ef37b8b3ea9def634c86",
+    "url": "./static/js/2.ef37b8b3.chunk.js"
+  },
+  {
+    "revision": "a178e178947684996785",
+    "url": "./static/js/runtime~main.a178e178.js"
+  },
+  {
+    "revision": "c9e15e34455a98537ef13c23c531f42b",
+    "url": "./index.html"
+  }
+];
